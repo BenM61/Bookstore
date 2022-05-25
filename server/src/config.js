7c138177c0b68a6d12ee4da0fb5c3dc0638c2381
BenM61 <benmassarano@mail.tau.ac.il>
2022-05-25 12:46:25 +0300

    changed collection name for this branch

--- server/src/config.js
+++ server/src/config.js
@@ -2,7 +2,7 @@ const config = {
   db: {
     DB_NAME: "BenMs-DB",
     BOOKS_COL_NAME: "Books-collection",
-    ReceiptS_COL_NAME: "Receipts-collection",
+    ReceiptS_COL_NAME: "Receipts-collection-buggy",
     PAGE_SIZE: 20
   },
   server: {
